b332f75dee67d19ab902f2b880f2830e8db6fc9b
ddrsmiLe <ddrsmile@gmail.com>
2017-03-09 23:40:26 +0900

    updated parser interface

--- src/utils/inputparser/IParser.java
+++ src/utils/inputparser/IParser.java
@@ -1,9 +1,9 @@
 package utils.inputparser;
 import java.util.ArrayList;
 
-public interface IParser<E> {
-    public ArrayList<E> parseDataAsNumber();
-    public ArrayList<E[]> parseDataAsList();
-    public ArrayList<ArrayList<E[]>> parseDataAsLists();
+public interface IParser<S, T> {
+    public ArrayList<S> parseDataAsNumber();
+    public ArrayList<T> parseDataAsList();
+    public ArrayList<ArrayList<T>> parseDataAsLists();
     //public ArrayList<ArrayList<E[][]>> parseDataAsMatrix();
 }

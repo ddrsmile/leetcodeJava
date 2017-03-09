23301cb8088576ba42cad230ca0cc05e2338b91e
Liu <nantsou.liu@rakuten.com>
2017-03-09 18:09:17 +0900

    rename inputhandler to inputparser

--- src/utils/inputhandler/IHandler.java
+++
@@ -1,9 +0,0 @@
-package utils.inputhandler;
-import java.util.ArrayList;
-
-public interface IHandler<T> {
-    public T[] GetDataAsSingleValue();
-    public ArrayList<T[]> GetDataAsSingleList();
-    public ArrayList<ArrayList<T[]>> GetDataAsLists();
-    public ArrayList<ArrayList<T[][]>> GetDataAsSingleMatrix();
-}

729603fe9687df2f6a0a2d787599c9a9026b50f2
ddrsmiLe <ddrsmile@gmail.com>
2017-03-09 23:40:48 +0900

    updated abstruct parser

--- src/utils/inputparser/AbstractParser.java
+++ src/utils/inputparser/AbstractParser.java
@@ -2,7 +2,7 @@ package utils.inputparser;
 import java.util.ArrayList;
 import java.io.BufferedReader;
 
-public abstract class AbstractParser<E> implements IParser<E> {
+public abstract class AbstractParser<S, T> {
     // variables
     protected String inputPath;
     protected BufferedReader br;
@@ -12,8 +12,9 @@ public abstract class AbstractParser<E> implements IParser<E> {
     protected AbstractParser(String inputPath) {this.inputPath = inputPath;}
 
     // protected methods
-    abstract protected E[] toList(String str);
-    abstract protected ArrayList<E[]> toLists(String str);
+    abstract protected S toNumber(String str);
+    abstract protected T toList(String str);
+    abstract protected ArrayList<T> toLists(String str);
 
     // public methods
     public void setInputPath(String inputPath) {this.inputPath = inputPath;}

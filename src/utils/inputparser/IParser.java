768e529da88f637b63f2bcafb5e49e6dac35f728
ddrsmiLe <ddrsmile@gmail.com>
2017-03-11 00:13:03 +0900

    changed method name from parseDataAsNumber to parseDataAsSingleNumber

--- src/utils/inputparser/IParser.java
+++ src/utils/inputparser/IParser.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 public interface IParser<S, T> {
     public void setInputPath(String inputPath);
 
-    public ArrayList<S> parseDataAsNumber();
+    public ArrayList<S> parseDataAsSingleValue();
     public ArrayList<T> parseDataAsList();
     public ArrayList<ArrayList<T>> parseDataAsLists();
     //public ArrayList<ArrayList<E[][]>> parseDataAsMatrix();
